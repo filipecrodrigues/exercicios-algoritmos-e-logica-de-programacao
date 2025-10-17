84ca5f1ad51d20f3e56d6bf553f866e9d14d8d47
Filipe <filipe.candido.rodrigues@gmail.com>
2025-10-16 23:16:42 -0300

    Adiciona exercício  Números Pares e Ímpares

---
+++ NumerosParesEImpares/Program.cs
@@ -0,0 +1,10 @@
+﻿using System; //importa biblioteca system, funções básicas
+
+class Program // classe program
+{
+    static void Main() //Ponto de entrada do programa
+    {
+        System.Console.WriteLine("Digite 10 números para verificar se são pares ou impares");
+    }
+    
+}
